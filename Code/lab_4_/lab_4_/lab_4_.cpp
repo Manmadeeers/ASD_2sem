6adb31844d9d20c42687ce1417655954fb1c568c
Manmad <philmade6@gmail.com>
2024-04-10 19:01:23 +0300

    lab4 fix

--- Code/lab_4_/lab_4_/lab_4_.cpp
+++ Code/lab_4_/lab_4_/lab_4_.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 void main()
 {
-	cout << "Welcome to FIT-Audio (kontora pidorasov)!" << endl;
+	cout << "Welcome to FIT-Audio (kontora Shimana)!" << endl;
 	int N;
 	cout << "Enter the amount of goods: ";
 	cin >> N;
 	int* arr_of_goods = new int[N];
 	cout << "Your cart: ";
 	for (int i = 0; i < N; i++) {
-		*(arr_of_goods + i) = rand() % 100+1;
+		*(arr_of_goods + i) = rand() % 10+1;
 		cout << *(arr_of_goods + i) << ' ';
 	}
 	cout << endl;
@@ -25,15 +25,15 @@ void main()
 			}
 		}
 	}
-	cout << "The final order: ";
+	cout << "Sorted version: ";
 	for (int i = 0; i < N; i++) {
 		cout << *(arr_of_goods + i) << ' ';
 	}
-	cout << endl;
-	cout << "Maximal amount of money spent: ";
+	// 
 	int Max_sum = 0;
-	for (int i = 0; i < N - 2; i++) {
+	int amm_of_dup = N / 2;
+	for (int i = 0; i < N - amm_of_dup; i++) {
 		Max_sum += *(arr_of_goods + i);
 	}
-	cout << Max_sum << endl;
+	cout << "Max sum: " << Max_sum << endl;
 }
